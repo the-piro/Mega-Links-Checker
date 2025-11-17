e86e782bc2800bc4fa6c0e7e50773d60afbf1dfe
the-piro <thezake0077@gmail.com>
2025-11-17 21:44:48 +0530

    Update cmds.py

--- plugins/cmds.py
+++ plugins/cmds.py
@@ -3,8 +3,9 @@ from pyrogram import Client, filters
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 from xtra import check_cmd, LINK_REGEX
 
-WELCOME = "Hello! Send me any MEGA link and I will check it."
-IMG = "https://i.ibb.co/xK56gh8W/photo-2025-11-14-13-10-23-7572567765697953804.jpg"
+WELCOME = "Hello {first}! Send me any MEGA link and I will check it."
+IMG = "https://envs.sh/xLo.jpg/HGBOTZ.jpg"
+MSG_EFFECT = 2  
 
 btns = InlineKeyboardMarkup(
     [[InlineKeyboardButton("Repo", url="https://github.com/XalFH/Mega-Links-Checker")]]
@@ -19,9 +20,17 @@ async def auto_check_mega(client, message):
 
 @Client.on_message(filters.command("start"))
 async def start_cmd(client, message):
-    await message.reply_photo(
-        IMG,
-        caption=WELCOME,
-        reply_markup=btns,
-        quote=True
+    first_name = message.from_user.first_name if message.from_user else "there"
+    username = f"@{message.from_user.username}" if getattr(message.from_user, "username", None) else ""
+    
+    start_caption = WELCOME.format(first=first_name)
+    if username:
+        start_caption += f"\n{username}"
+
+    await client.send_photo(
+        chat_id=message.chat.id,
+        photo=IMG,
+        caption=start_caption,
+        message_effect_id=MSG_EFFECT,
+        reply_markup=btns
     )
