aec8e2194c92bb8c2420eebf56fd93fc54344e56
the-piro <thezake0077@gmail.com>
2025-11-17 18:11:38 +0530

    Update and rename main.py to bot.py

---
+++ bot.py
@@ -0,0 +1,24 @@
+from pyrogram import Client
+from uvloop import install
+import asyncio
+from config import API_ID, API_HASH, BOT_TOKEN
+
+install()
+
+try:
+    asyncio.get_event_loop()
+except RuntimeError:
+    loop = asyncio.new_event_loop()
+    asyncio.set_event_loop(loop)
+
+Bot = Client(
+    "MegaCheckerBot",
+    api_id=API_ID,
+    api_hash=API_HASH,
+    bot_token=BOT_TOKEN,
+    plugins={"root": "plugins"},
+    workers=50
+)
+
+Bot.start()
+Bot.me = Bot.get_me()
