e3769b32e6a4db1ac1c4a9fabff0e9d2a902af92
the-piro <thezake0077@gmail.com>
2025-11-17 21:55:24 +0530

    Update cmds.py

--- plugins/cmds.py
+++ plugins/cmds.py
@@ -5,6 +5,7 @@ from xtra import check_cmd, LINK_REGEX
 
 WELCOME = "Hello! Send me any MEGA link and I will check it."
 IMG = "https://i.ibb.co/xK56gh8W/photo-2025-11-14-13-10-23-7572567765697953804.jpg"
+MSG_EFFECT = 5046509860389126442
 
 btns = InlineKeyboardMarkup(
     [[InlineKeyboardButton("Repo", url="https://github.com/XalFH/Mega-Links-Checker")]]
@@ -23,5 +24,6 @@ async def start_cmd(client, message):
         IMG,
         caption=WELCOME,
         reply_markup=btns,
+        message_effect_id=MSG_EFFECT, 
         quote=True
                    )
