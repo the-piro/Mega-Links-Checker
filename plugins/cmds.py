a6f5808a7a622731ff6aa2878dbb6d9d5227f0ae
the-piro <thezake0077@gmail.com>
2025-11-17 19:00:48 +0530

    Update cmds.py

--- plugins/cmds.py
+++ plugins/cmds.py
@@ -1,3 +1,9 @@
+import re
+from pyrogram import Client, filters
+from xtra import check_cmd, LINK_REGEX
+
+WELCOME = "Hello! Send me any MEGA link and I will check it."
+
 @Client.on_message(filters.text | filters.caption)
 async def auto_check_mega(client, message):
     if message.text and message.text.startswith("/start"):
@@ -8,7 +14,6 @@ async def auto_check_mega(client, message):
         return
 
     await check_cmd(client, message)
-
 
 @Client.on_message(filters.command("start"))
 async def start_cmd(client, message):
